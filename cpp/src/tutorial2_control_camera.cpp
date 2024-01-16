87bfc26d95dce487c7d84e901da268a21a7c924f
Momoko Kono <momoko.kono@fixstars.com>
2024-01-17 02:44:02 +0900

    enabled the control Gain and ExposureTime

---
+++ cpp/src/tutorial2_control_camera.cpp
@@ -0,0 +1,128 @@
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+
+#include <ion/ion.h>
+
+#include <exception>
+#include <string>
+#include <map>
+
+using namespace ion;
+
+std::map<std::string, int32_t> num_bit_shift_map{
+  {"Mono8", 0}, {"Mono10", 6}, {"Mono12", 4}};
+std::map<std::string, int32_t> opencv_mat_type{
+  {"Mono8", CV_8UC1}, {"Mono10", CV_16UC1}, {"Mono12", CV_16UC1}};
+std::map<std::string, std::string> bb_name{
+  {"Mono8", "image_io_u3v_cameraN_u8x2"}, 
+  {"Mono10", "image_io_u3v_cameraN_u16x2"}, 
+  {"Mono12", "image_io_u3v_cameraN_u16x2"}};
+
+int positive_pow(int base, int expo){
+  if (expo <= 0){
+      return 1;
+  }
+  if (expo == 1){
+      return base;
+  }else{
+      return base * positive_pow(base, expo-1);
+  }
+}
+
+double gain0 = 35.0;
+double exposuretime0 = 50.0;
+double gain1 = 47.0;
+double exposuretime1 = 100.0;
+
+template<typename T>
+int video(int width, int height, std::string pixel_format, int num_device){
+    // pipeline setup
+    Builder b;
+    b.set_target(ion::get_host_target());
+    b.with_bb_module("ion-bb");
+
+    // add BB to pipeline
+    Node n;
+    if (num_device == 1){
+      n = b.add(bb_name[pixel_format])(&gain0, &exposuretime0)
+        .set_param(
+          Param("num_devices", num_device),
+          Param("frame_sync", true),
+          Param("realtime_diaplay_mode", false),
+          Param("enable_control", true),
+          Param("gain_key", "Gain"),
+          Param("exposure_key", "ExposureTime")
+        );
+    }else if (num_device == 2){
+      n = b.add(bb_name[pixel_format])(&gain0, &exposuretime0, &gain1, &exposuretime1)
+        .set_param(
+          Param("num_devices", num_device),
+          Param("frame_sync", true),
+          Param("realtime_diaplay_mode", false),
+          Param("enable_control", true),
+          Param("gain_key", "Gain"),
+          Param("exposure_key", "ExposureTime")
+        );
+    }else{
+      throw("Set the same number of gains and exposure times as the number of devices.");
+    }
+    
+    // portmapping from output port to buffer
+    std::vector< int > buf_size = std::vector < int >{ width, height };
+    if (pixel_format == "RGB8"){
+        buf_size.push_back(3);
+    }
+    std::vector<Halide::Buffer<T>> output;
+    for (int i = 0; i < num_device; ++i){
+      output.push_back(Halide::Buffer<T>(buf_size));
+    }
+    n["output"].bind(output);
+
+    int loop_num = 100;
+    for (int i = 0; i < loop_num; ++i)
+    {
+      // JIT compilation and execution of pipelines with Builder.
+      try {
+          b.run();
+      }catch(std::exception& e){
+          // e.what() shows the error message if pipeline build/run was failed.
+          std::cerr << "Failed to build pipeline" << std::endl;
+          std::cerr << e.what() << std::endl;
+          exit(1);
+      }
+
+      // Convert the retrieved buffer object to OpenCV buffer format.
+      for (int i = 0; i < num_device; ++i){
+        cv::Mat img(height, width, opencv_mat_type[pixel_format], output[i].data());
+        img *= positive_pow(2, num_bit_shift_map[pixel_format]);
+        cv::imshow("image" + std::to_string(i), img);
+      }
+
+      // Wait for 1ms
+      cv::waitKey(1);
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[])
+{
+  try{
+    int32_t width = 1920;
+    int32_t height = 1080;
+    int32_t num_device = 1;
+    std::string pixelformat = "Mono8";
+
+    if (pixelformat == "Mono8"){
+      int ret = video<uint8_t>(width, height, pixelformat, num_device);
+    }else{
+      int ret = video<uint16_t>(width, height, pixelformat, num_device);
+    }
+
+
+  }catch(std::exception& e){
+    std::cerr << e.what() << std::endl;
+    exit(1);
+  }
+  return 0;
+}
