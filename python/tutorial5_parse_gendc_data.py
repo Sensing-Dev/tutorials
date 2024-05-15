e52306aa3aded833b1818f085dfeabee1f153e74
xinyu.li <xinyu.li@fixstars.com>
2024-05-15 13:46:40 -0700

    update python t5

--- python/tutorial5_parse_gendc_data.py
+++ python/tutorial5_parse_gendc_data.py
@@ -4,64 +4,68 @@ import os
 import numpy as np
 import cv2
 
-from  gendc_python.gendc_separator import descriptor as gendc
+from gendc_python.gendc_separator import descriptor as gendc
 
-GDC_INTENSITY   = 0x0000000000000001
+GDC_INTENSITY = 1
 
 if __name__ == "__main__":
 
-    directory_name = "tutorial_save_gendc_XXXXXXXXXXXXXX"
+    directory_name = "tutorial_save_gendc_xxxxxxxxxxxx"
     prefix = "gendc0-"
 
     num_device = 1
 
     if not (os.path.exists(directory_name) and os.path.isdir(directory_name)):
-        raise Exception("Directory" + directory_name + " does not exist")        
+        raise Exception("Directory" + directory_name + " does not exist")
 
     bin_files = [f for f in os.listdir(directory_name) if f.startswith(prefix) and f.endswith(".bin")]
     bin_files = sorted(bin_files, key=lambda s: int(s.split('-')[-1].split('.')[0]))
-
+    if len(bin_files) == 0:
+        raise Exception("No bin files with prefix {}  detected".format(prefix))
     for bf in bin_files:
         bin_file = os.path.join(directory_name, bf)
 
         with open(bin_file, mode='rb') as ifs:
-            filecontent = ifs.read()   
+            filecontent = ifs.read()
             cursor = 0
 
             while cursor < len(filecontent):
-                try:
-                    # TODO return NULL for non-gendc format
-                    gendc_descriptor = gendc.Container(filecontent[cursor:])
 
-                    # get GenDC container information
-                    descriptor_size = gendc_descriptor.get("DescriptorSize")
-                    print("GenDC Descriptor size:", descriptor_size)
-                    data_size = gendc_descriptor.get("DataSize")
-                    print("GenDC Data size:", data_size)
+                # TODO return NULL for non-gendc format
+                gendc_container = gendc.Container(filecontent[cursor:])
 
-                    # get first available component
-                    image_component = gendc_descriptor.get_first_get_datatype_of(GDC_INTENSITY)
-                    print("First available image data component is Comp", image_component)
-                    print("\tData dimension: ", gendc_descriptor.get("PartCount", image_component))
-                    print("\tData size:", gendc_descriptor.get("DataSize", image_component, 0))
-                    print("\tData offset:", gendc_descriptor.get("DataOffset", image_component, 0))
+                # get GenDC container information
+                descriptor_size = gendc_container.get_descriptor_size()
+                print("GenDC Descriptor size:", descriptor_size)
+                data_size = gendc_container.get_data_size()
+                print("GenDC Data size:", data_size)
 
-                    typespecific3 = gendc_descriptor.get("TypeSpecific", image_component, 0)[2]
-                    print("Framecount: ", int.from_bytes(typespecific3.to_bytes(8, 'little')[0:4], "little"))
+                # get first available component
+                image_component_idx = gendc_container.get_1st_component_idx_by_typeid(GDC_INTENSITY)
+                print("First available image data component is Comp", image_component_idx)
+                image_component = gendc_container.get_component_by_idx(image_component_idx)
+                part_count = image_component.get_part_count()
+                print("\tData Channel: ", part_count)
 
-                    cursor = cursor + gendc_descriptor.get_container_size()
-                    print()
-
-                except:
-                    print("========================================")
-                    print("This is not GenDC Format data.")
-                    print("If you save this with image_io_binarysaver_u{}x{} BB, the data structure is")
-                    print()
-                    print("| framecount0 | imagedata0 | framecount1 | imagedata1 | framecount2 | imagedata2 | ... |")
-                    print()
-                    print("framecount is 4 byte-length, and imagedata size is width * height * byte-depth * number of channel, ")
-                    print("which you may be able to find/calculate from config.json file.")
-                    print("Note that framecount is not frame id. Some device may not have this number, and if so, it is filled with 0.")
-                    print("========================================")
-                    raise Exception("This is not GenDC Format")
-
+                for part_id in range(part_count):
+                    part = image_component.get_part_by_idx(part_id)
+                    dimension = part.get_dimension()
+                    print("\tDimension: ", "x".join(str(v) for v in dimension))
+                    w_h_c = part_count
+                    for d in dimension:
+                        w_h_c *= d
+                    byte_depth = int(data_size / w_h_c)
+                    print("\tByte-depth of image", byte_depth)
+                    typespecific3 = part.get("TypeSpecific")[2]
+                    print("Frame count: ", int.from_bytes(typespecific3.to_bytes(8, 'little')[0:4], "little"))
+                    cursor = cursor + gendc_container.get_container_size()
+                    width = dimension[0]
+                    height = dimension[1]
+                    if byte_depth == 1:
+                        image = np.frombuffer(part.get_data(), dtype=np.uint8).reshape((height, width))
+                    elif byte_depth == 2:
+                        image = np.frombuffer(part.get_data(), dtype=np.uint16).reshape((height, width))
+                    user_input = -1
+                    cv2.imshow("img", image)
+                    cv2.waitKey(1)
+            cv2.destroyAllWindows()
