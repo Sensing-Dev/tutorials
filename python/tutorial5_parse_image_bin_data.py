d9b271c7d7e4d64c2d4298e2cbea2b66bfdc2147
Fixstars-momoko <momoko.kono@fixstars.com>
2024-06-21 13:10:09 -0700

    WIP: confirmed with 2 non-gendc device

---
+++ python/tutorial5_parse_image_bin_data.py
@@ -0,0 +1,64 @@
+import re
+import os
+
+import numpy as np
+import cv2
+
+import json
+
+import struct
+
+Mono8 = 0x01080001
+Mono10 = 0x01100003
+Mono12 = 0x01100005
+RGB8 = 0x02180014
+BGR8 = 0x02180015
+
+if __name__ == "__main__":
+
+    directory_name = "tutorial_save_image_bin_XXXXXXXXXXXXXXXX"
+    prefix = "image0-"
+
+    num_device = 1
+
+    if not (os.path.exists(directory_name) and os.path.isdir(directory_name)):
+        raise Exception("Directory " + directory_name + " does not exist")
+    
+    # image info from image0-config.json
+    f = open(os.path.join(directory_name, prefix + "config.json"))
+    config = json.loads(f.read())
+    f.close()
+
+    w = config["width"]
+    h = config["height"]
+    d = 2 if config["pfnc_pixelformat"] == Mono10 or config["pfnc_pixelformat"] == Mono12 \
+        else 1
+    c = 3 if config["pfnc_pixelformat"] == RGB8 or config["pfnc_pixelformat"] == BGR8 \
+        else 1
+    framesize = w * h * d * c
+
+    np_dtype = np.uint8 if d == 1 else np.uint16 
+
+    bin_files = [f for f in os.listdir(directory_name) if f.startswith(prefix) and f.endswith(".bin")]
+    bin_files = sorted(bin_files, key=lambda s: int(s.split('-')[-1].split('.')[0]))
+    if len(bin_files) == 0:
+        raise Exception("No bin files with prefix {}  detected".format(prefix))
+    for bf in bin_files:
+        bin_file = os.path.join(directory_name, bf)
+
+        with open(bin_file, mode='rb') as ifs:
+            filecontent = ifs.read()
+            cursor = 0
+
+            while cursor < len(filecontent):
+                framecount = struct.unpack('I', filecontent[cursor:cursor+4])[0]
+                image = np.frombuffer(filecontent[cursor+4:cursor+4+framesize], dtype=np_dtype).reshape((h, w))
+
+                print(framecount)
+
+                cv2.imshow("First available image component", image)
+                cv2.waitKey(1)
+
+                cursor = cursor + 4 + framesize
+    cv2.destroyAllWindows()
+
