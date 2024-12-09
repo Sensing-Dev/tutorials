4f75f402274f8f2ea93d21a55c513cfb34eea5b8
fixstars_xinyu <143661893+xinyuli1204@users.noreply.github.com>
2024-12-09 11:48:45 -0800

    add color mode and 16 bits

--- gstreamer/tutorial1_display.py
+++ gstreamer/tutorial1_display.py
@@ -1,20 +1,50 @@
 import os
-if os.name == 'nt':
+
+if os.name == 'nt':  # windows
     os.add_dll_directory(os.path.join(os.environ["SENSING_DEV_ROOT"], "bin"))
 # linux: please install sensing-dev c++ version and export GST_PLUGIN_PATH=/opt/sensing-dev/lib/x86_64-linux-gnu/gstreamer-1.0
 import cv2
 
 if __name__ == "__main__":
+
     camera_name = ''
-    # camera_name = 'sensor name here'
-    pixelformat = 'GRAY8'
+    # camera_name = 'camera-name'
+    pixelformat = 'Mono8'
+    # pixelformat = 'BayerBG8'
     width = 1920
     height = 1080
     framerate = 60
+    is_color = False
+    required_bits = 8 if pixelformat == "Mono8" or pixelformat == "RGB8" \
+        else 16 if pixelformat == "Mono10" or pixelformat == "Mono12" \
+        else 8
 
-    pipeline = 'aravissrc camera-name="{}" ! \
-    gendcseparator name=sep ! queue ! fakesink \
-    sep.component_src0 ! queue ! video/x-raw,format={},width={},height={},framerate={}/1 ! videoconvert ! appsink'.format(camera_name, pixelformat, width, height, framerate)
+    num_bit_shift = 0 if pixelformat == "Mono8" or pixelformat == "RGB8" \
+        else 4 if pixelformat == "Mono12" \
+        else 6 if pixelformat == "Mono10" \
+        else 0
+    if pixelformat.startswith("Mono"):
+        is_color = False
+    else:
+        is_color = True
+
+    if is_color:
+        pipeline = 'aravissrc camera-name="{}" ! \
+        gendcseparator name=sep ! queue ! fakesink \
+        sep.component_src0 ! queue ! video/x-bayer,format=bggr,width={},height={},framerate={}/1 ! bayer2rgb ! videoconvert ! appsink'.format(
+            camera_name, width, height, framerate)
+    else:
+        if required_bits == 8:
+            pipeline = 'aravissrc camera-name="{}" ! \
+            gendcseparator name=sep ! queue ! fakesink \
+            sep.component_src0 ! queue ! video/x-raw,format=GRAY8,width={},height={},framerate={}/1 ! videoconvert ! appsink'.format(
+                camera_name, width, height, framerate)
+        else:
+            pipeline = 'aravissrc camera-name="{}" ! \
+                       gendcseparator name=sep ! queue ! fakesink \
+                       sep.component_src0 ! queue ! video/x-raw,format=GRAY16_LE,depth={},width={},height={},framerate={}/1 ! videoconvert ! appsink'.format(
+                camera_name, required_bits-num_bit_shift, width, height, framerate)
+
 
     print(pipeline)
 
@@ -28,7 +58,6 @@ if __name__ == "__main__":
         if not ret:
             print("Can't receive frame")
             break
-
         cv2.imshow('opencv with gstreamer test', frame)
         user_input = cv2.waitKeyEx(1)
     cv2.destroyAllWindows()
